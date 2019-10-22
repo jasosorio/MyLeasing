f115cd3c20d1c4a5284ed7c0e996b74e4cf902a4
Jasson Mauricio Osorio Bedoya <jasosorio49@hotmail.com>
2019-10-22 08:19:41 -0500

    Create login with prism

--- MyLeasing.Web/Data/Entities/PropertyImage.cs
+++ MyLeasing.Web/Data/Entities/PropertyImage.cs
@@ -16,7 +16,7 @@ namespace MyLeasing.Web.Data.Entities
         // TODO: Change the path when publish
         public string ImageFullPath => string.IsNullOrEmpty(ImageUrl) 
             ? null 
-            : $"https://TBD.azurewebsites.net{ImageUrl.Substring(1)}";
+            : $"https://myleasingjasson.azurewebsites.net{ImageUrl.Substring(1)}";
 
         public Property Property { get; set; }
     }
