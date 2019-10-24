7fc4a6d34e17b7160e69f4bbc4f12d4b28853327
Jasson Mauricio Osorio Bedoya <jasosorio49@hotmail.com>
2019-10-24 14:53:15 -0500

    List Properties

--- MyLeasing.Command/Models/PropertyResponse.cs
+++ MyLeasing.Command/Models/PropertyResponse.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace MyLeasing.Common.Models
 {
@@ -29,5 +30,9 @@ namespace MyLeasing.Common.Models
         public ICollection<PropertyImageResponse> PropertyImages { get; set; }
 
         public ICollection<ContractResponse> Contracts { get; set; }
+
+        public string FirsImage => PropertyImages == null || PropertyImages.Count == 0
+                ? "https://myleasingjasson.azurewebsites.net/images/Properties/noImage.jpg"
+                : PropertyImages.FirstOrDefault().ImageUrl;
     }
 }
--- MyLeasing.Prism/MyLeasing.Prism/ViewModels/PropertiesPageViewModel.cs
+++ MyLeasing.Prism/MyLeasing.Prism/ViewModels/PropertiesPageViewModel.cs
@@ -4,6 +4,7 @@ using Prism.Mvvm;
 using Prism.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace MyLeasing.Prism.ViewModels
@@ -11,12 +12,18 @@ namespace MyLeasing.Prism.ViewModels
     public class PropertiesPageViewModel : ViewModelBase
     {
         private OwnerResponse _owner;
+        private ObservableCollection<PropertyResponse> _properties;
         public PropertiesPageViewModel(
             INavigationService navigationService) : base(navigationService)
         {
             Title = "Properties";
         }
 
+        public ObservableCollection<PropertyResponse> Properties
+        {
+            get => _properties;
+            set => SetProperty(ref _properties, value);
+        }
         public override void OnNavigatedTo(INavigationParameters parameters)
         {
             base.OnNavigatedTo(parameters);
@@ -25,6 +32,7 @@ namespace MyLeasing.Prism.ViewModels
             {
                 _owner = parameters.GetValue<OwnerResponse>("owner");
                 Title = $"Properties of: {_owner.FullName}";
+                Properties = new ObservableCollection<PropertyResponse>(_owner.Properties);
             } 
         }
     }
