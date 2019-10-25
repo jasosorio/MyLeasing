134a99522baebe5e2b23b21e1ab87be69464e554
Jasson Mauricio Osorio Bedoya <jasosorio49@hotmail.com>
2019-10-25 13:22:44 -0500

    Show property details

---
+++ MyLeasing.Prism/MyLeasing.Prism/ViewModels/PropertyItemViewModel.cs
@@ -0,0 +1,30 @@
+﻿using MyLeasing.Common.Models;
+using Prism.Commands;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLeasing.Prism.ViewModels
+{
+    public class PropertyItemViewModel : PropertyResponse
+    {
+        private readonly INavigationService _navigationService;
+        private DelegateCommand _selectPropertyCommand;
+
+        public PropertyItemViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+        public DelegateCommand SelectPropertyCommand => _selectPropertyCommand ?? (_selectPropertyCommand = new DelegateCommand(SelectProperty));
+        private async void SelectProperty()
+        {
+            var parameters = new NavigationParameters
+            {
+                { "Property", this }
+            };
+            await _navigationService.NavigateAsync("PropertyPage", parameters);
+        }
+    }
+}
---
+++ MyLeasing.Prism/MyLeasing.Prism/ViewModels/PropertyPageViewModel.cs
@@ -0,0 +1,37 @@
+﻿using MyLeasing.Common.Models;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyLeasing.Prism.ViewModels
+{
+    public class PropertyPageViewModel : ViewModelBase
+    {
+        private PropertyResponse _property;
+        public PropertyPageViewModel(
+            INavigationService navigationService) : base(navigationService)
+        {
+            Title = "Property";
+        }
+
+        public PropertyResponse Property
+        {
+            get => _property;
+            set => SetProperty(ref _property, value);
+        }
+
+        public override void OnNavigatedTo(INavigationParameters parameters)
+        {
+            base.OnNavigatedTo(parameters);
+
+            if (parameters.ContainsKey("Property"))
+            {
+                Property = parameters.GetValue<PropertyResponse>("Property");
+                Title = $"Property : {Property.Neighborhood}";
+            }
+        }
+    }
+}
